15556787b1075b5add9090359d49830d494a6aea
Leroy Salih <Leroy.Salih@gmail.com>
2020-10-14 12:01:52 +0300

    First CTF

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+
+
+console.log(process.env.PORT)
+
+app.get('/code/:code', (req, res) => {
+  
+  code = req.params.code
+  if (code == "5467"){
+    res.send('Hacked!  You have access to the flag.')
+  } else {
+    res.send('Invalid Code')
+  }
+  
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on ${process.env.PORT}`)
+});
